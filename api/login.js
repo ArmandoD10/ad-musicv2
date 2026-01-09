1d2941020a70353d72ede2b7fcb1cd7c9c9555eb
Armando Diaz <armandodiaz1020158@gmail.com>
2026-01-09 10:36:50 -0400

    Fix login: variables email/password y errores frontend

--- api/login.js
+++ api/login.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
         const { email, password } = req.body;
 
         const result = await db.execute({
-            sql: "SELECT * FROM usuarios WHERE email = ?",
+            sql: "SELECT * FROM usuario WHERE correo = ?",
             args: [email]
         });
 
